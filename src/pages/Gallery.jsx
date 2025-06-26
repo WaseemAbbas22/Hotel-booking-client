c1d243e8ca0ccf91ca535633efaee9e518f25232
Waseem Abbas <Waseemabbasce22@gmail.com>
2025-06-26 16:46:41 +0500

    Fix: moved assets to public for Vercel image support

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
     <>
     
     <HeroSection
-      image="/src/assets/IMG_E6024.jpg"
+      image="/assets/IMG_E6024.jpg"
       title="Discover The Beauty of Tarishing"
       subtitle="From humble beginnings to a mountain paradise"
     />
